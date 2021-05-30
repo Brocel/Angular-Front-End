7a0977366429e29b81a9b1fabceeb31916d16969
Brocel <geenens.prof@gmail.com>
2021-05-30 12:58:11 -0300

    Edit model user.ts

---
+++ src/app/model/user.ts
@@ -0,0 +1,14 @@
+export class User {
+
+  id: number;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  age: number;
+  country: string;
+  state: string;
+  city: string;
+  password: string;
+  
+}
