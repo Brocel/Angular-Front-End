978d94c2f2b831c75749002c67d0f593cb54b0a0
Brocel <geenens.prof@gmail.com>
2021-05-31 14:29:59 -0300

    Refactoring Modules : edit user service -> consume rest api

--- src/app/core/service/user.service.ts
+++ src/app/core/service/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserImpl } from '@app/shared/model/implementation/user-impl';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<UserImpl[]> {
+    return this.http.get<UserImpl[]>('http://localhost:8080/api/users');
+  }
 }
