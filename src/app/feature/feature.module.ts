a4447be65b43a8e2a46da0c12fd5f391ad5396f9
Brocel <geenens.prof@gmail.com>
2021-05-31 10:06:19 -0300

    Refactoring Modules : adding home modules and some features

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeatureRoutingModule } from './feature-routing.module';
+import { UserInfoModule } from './user-info/user-info.module';
+import { CharacterListModule } from './character-list/character-list.module';
+import { GameListModule } from './game-list/game-list.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    FeatureRoutingModule
+    FeatureRoutingModule,
+    UserInfoModule,
+    CharacterListModule,
+    GameListModule
   ]
 })
 export class FeatureModule { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
+import { UserListModule } from './user-list/user-list.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    LoginModule,
+    RegisterModule,
+    UserListModule
   ]
 })
 export class HomeModule { }
