87f3cb9ad1b747571cb4bf584164d9b7aa091e11
Brocel <geenens.prof@gmail.com>
2021-05-31 14:36:25 -0300

    Refactoring Modules : edit userlist component

--- src/app/home/user-list/user-list.component.ts
+++ src/app/home/user-list/user-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "@app/core/service/user.service";
+import { UserImpl } from '@app/shared/model/implementation/user-impl';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
 })
-export class UserListComponent implements OnInit {
+export class UserComponent implements OnInit {
 
-  constructor() { }
+  users: UserImpl[];
+
+  constructor(private userService: UserService) {
+    this.users = [];
+  }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe((data: UserImpl[]) => {
+      console.log(data);
+      this.users = data;
+    });
   }
 
 }
