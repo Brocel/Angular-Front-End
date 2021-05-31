e0c25215f90cf831a744ba91ac7d725b33c691bc
Brocel <geenens.prof@gmail.com>
2021-05-31 11:09:09 -0300

    Refactoring Modules : interface + model -> user

---
+++ src/app/shared/model/implementation/user-impl.ts
@@ -0,0 +1,39 @@
+import { User } from '@app/shared/model/interface/user';
+
+export class UserImpl implements User {
+
+  readonly id: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  age: number;
+  country: string;
+  state: string;
+  city: string;
+  password: string;
+
+  constructor(options:{
+    id?: string;
+    firstName?: string;
+    lastName?: string;
+    userName?: string;
+    email?: string;
+    age?: number;
+    country?: string;
+    state?: string;
+    city?: string;
+    password?: string;
+  } = {}) {
+    this.id = options.id || '';
+    this.firstName = options.firstName || '';
+    this.lastName = options.lastName || '';
+    this.userName = options.userName || '';
+    this.email = options.email || '';
+    this.age = options.age || 0;
+    this.country = options.country || '';
+    this.state = options.state || '';
+    this.city = options.city || '';
+    this.password = options.password || '';
+  }
+}
---
+++ src/app/shared/model/interface/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  age: number;
+  country: string;
+  state: string;
+  city: string;
+  password: string;
+}
