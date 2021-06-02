484549d2eee0f72f3ed7affc9677a8697deedb70
Brocel <geenens.prof@gmail.com>
2021-06-02 17:54:44 -0300

    starting dev login method...

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedModule } from '@app/shared/shared.module';
+//import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  signInForm!: FormGroup;
+  errorMessage!: string;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,/*private authService: AuthService,*/private router: Router) {  }
+
+  ngOnInit() {
+    this.initForm();
   }
 
+  initForm() {
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+
+  onSubmit() {
+
+    try {
+      const email = this.signInForm.get('email')?.value;
+      const password = this.signInForm.get('password')?.value;
+    } catch (e) {
+      console.log(e);
+    }
+
+
+    // this.authService.signInUser(email, password).then(
+    //   () => {
+    //     this.router.navigate(['/protected/table-jeu']);
+    //   },
+    //   (error) => {
+    //     this.errorMessage = error;
+    //   }
+    // );
+  }
 }
