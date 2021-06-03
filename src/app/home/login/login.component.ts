bf48c36182fa548df1455bc5c37b8349763683db
Brocel <geenens.prof@gmail.com>
2021-06-03 14:32:32 -0300

    adding services, edit login comp.ts

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,49 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { SharedModule } from '@app/shared/shared.module';
-//import { AuthService } from 'src/app/core/services/auth.service';
-import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
+import { AuthService, AlertService } from '@app/core/services/*';
+
+@Component({ templateUrl: 'login.component.html' })
 export class LoginComponent implements OnInit {
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
 
-  signInForm!: FormGroup;
-  errorMessage!: string;
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
 
-  constructor(private formBuilder: FormBuilder,/*private authService: AuthService,*/private router: Router) {  }
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
 
-  ngOnInit() {
-    this.initForm();
-  }
-
-  initForm() {
-    this.signInForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
-    });
-  }
-
-  onSubmit() {
-
-    try {
-      const email = this.signInForm.get('email')?.value;
-      const password = this.signInForm.get('password')?.value;
-    } catch (e) {
-      console.log(e);
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
 
-    // this.authService.signInUser(email, password).then(
-    //   () => {
-    //     this.router.navigate(['/protected/table-jeu']);
-    //   },
-    //   (error) => {
-    //     this.errorMessage = error;
-    //   }
-    // );
-  }
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.login(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
 }
