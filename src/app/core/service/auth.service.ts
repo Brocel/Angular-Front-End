9c4e4d5ed95828b7980181187374f1a8302ef123
Brocel <geenens.prof@gmail.com>
2021-06-21 09:55:03 -0300

    create and edi jwt client service, empty auth service

--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -6,17 +6,5 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) { }
 
-
-  public generateToken(request:any) {
-    return this.httpClient.post<string>("http://localhost:8080/authenticate", request, {  responseType: 'text' as 'json' });
-  }
-
-
-  public welcome(token:any) {
-    let tokenStr = 'Bearer ' + token;
-    const headers = new HttpHeaders().set('Authorization', tokenStr);
-    return this.httpClient.get<string>("http://localhost:8080/", {headers, responseType: 'text' as 'json' });
-  }
 }
