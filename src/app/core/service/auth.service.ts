6acac1f5e1c079218b371c94215efb7e1e3a1a32
Brocel <geenens.prof@gmail.com>
2021-06-21 09:27:49 -0300

    edit auth service & login comp

--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+
+  public generateToken(request:any) {
+    return this.httpClient.post<string>("http://localhost:8080/authenticate", request, {  responseType: 'text' as 'json' });
+  }
+
+
+  public welcome(token:any) {
+    let tokenStr = 'Bearer ' + token;
+    const headers = new HttpHeaders().set('Authorization', tokenStr);
+    return this.httpClient.get<string>("http://localhost:8080/", {headers, responseType: 'text' as 'json' });
+  }
 }
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -16,6 +16,11 @@ export class LoginComponent implements OnInit {
     loading = false;
     submitted = false;
     returnUrl?: string;
+    authRequest:any={
+    "userName":"",
+    "password":""
+    };
+    response:any;
 
     constructor(
         private formBuilder: FormBuilder,
@@ -26,17 +31,30 @@ export class LoginComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-        this.form = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
 
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+      this.form = this.formBuilder.group({
+          username: ['', Validators.required],
+          password: ['', Validators.required]
+      });
+
+      // get return url from route parameters or default to '/'
+      this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
     }
 
     // convenience getter for easy access to form fields
     get f() { return this.form?.controls; }
+
+    // Jwt auth
+    public getAccessToken(authRequest:any){
+      let resp=this.authService.generateToken(authRequest);
+      resp.subscribe(data=>this.accessApi(data));
+    }
+    
+    public accessApi(token:any){
+      let resp=this.authService.welcome(token);
+      resp.subscribe(data=>this.response=data);
+    }
 
     onSubmit() {
         this.submitted = true;
