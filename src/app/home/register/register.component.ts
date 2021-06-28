036e0bfbbcf40e7fc7d2d92b51ecf2ed6d11475a
Brocel <geenens.prof@gmail.com>
2021-06-28 12:23:29 -0300

    populate register comp.ts

--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@app/core/service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    firstName: null,
+    lastName: null,
+    userName: null,
+    email: null,
+    age: null,
+    country: null,
+    state: null,
+    city: null,
+    password: null
+  };
+
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const { username, email, password } = this.form;
+
+    this.authService.register(username, email, password).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
   }
 
 }
